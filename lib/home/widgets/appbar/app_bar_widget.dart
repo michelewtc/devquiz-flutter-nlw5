f277e68d388a020f2e13b9a617bbffc5aec15dea
michelewtc <michelechan1996@gmail.com>
2021-04-30 18:38:10 -0300

    first commit

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,40 @@
+import 'package:dev_quiz/core/app_gradients.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+                height: 250,
+                decoration: BoxDecoration(gradient: AppGradients.linear),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "michelewtc",
+                              style: AppTextStyles.titleBold,
+                            )
+                          ])),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/71111468?v=4"),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )));
+}
