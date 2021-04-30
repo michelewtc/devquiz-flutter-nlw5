7bf1f49a36055b0d6cd672c04e35dbf7bd7ffcae
michelewtc <michelechan1996@gmail.com>
2021-04-30 20:00:12 -0300

    aula 01

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,28 +1,39 @@
-import 'package:dev_quiz/core/app_gradients.dart';
-import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
       : super(
-            preferredSize: Size.fromHeight(250),
-            child: Container(
-                height: 250,
-                decoration: BoxDecoration(gradient: AppGradients.linear),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 270,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text.rich(TextSpan(
+                      Text.rich(
+                        TextSpan(
                           text: "Olá, ",
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
-                              text: "michelewtc",
+                              text: "Michele",
                               style: AppTextStyles.titleBold,
-                            )
-                          ])),
+                            ),
+                          ],
+                        ),
+                      ),
                       Container(
                         width: 58,
                         height: 58,
@@ -30,11 +41,20 @@ class AppBarWidget extends PreferredSize {
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
                             image: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/71111468?v=4"),
+                              'https://avatars.githubusercontent.com/u/71111468?v=4',
+                            ),
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
-                )));
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
+            ),
+          ),
+        );
 }
