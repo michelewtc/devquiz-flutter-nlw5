5012d5763ec6e9795a8f3d1e6b90f86684fa735b
michelewtc <michelechan1996@gmail.com>
2021-05-05 14:54:13 -0300

    QuizWidget & AnswerWidget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:dev_quiz/home/home_page.dart';
+import 'package:dev_quiz/challenge/challenge_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/core/app_images.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -43,14 +44,10 @@ class QuizCardWidget extends StatelessWidget {
                 ),
               ),
               Expanded(
-                flex: 3,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
-              )
+                  flex: 3,
+                  child: ProgressIndicatorWidget(
+                    value: 0.3,
+                  ))
             ],
           )
         ],
